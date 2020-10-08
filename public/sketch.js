959551adcbcb3982f55d3c98b64a28e6072e4167
Vu Doan <victordoan1708@gmail.com>
2020-10-08 01:37:46 +0100

    CREATED A BAR CHART. DESTROYED ONE WHEN CLOSED. MOBILE RESPONSIVENESS TO SUIT THE BAR CHART AT DIFFERENT SIZES. display more recipes, search a plant

--- public/sketch.js
+++ public/sketch.js
@@ -4,7 +4,9 @@ const windows = document.getElementById('windowProperties'),
       pages = document.getElementById('pages'),
       table_wrapper = document.getElementById('table_wrapper'),
       table_body = document.getElementById('table_body'),
-      loading = document.getElementById('loading');
+      loading = document.getElementById('loading'),
+      nutrientsChart = document.getElementById('nutrientsChart');
+let myChart = null;
 
 // RESTRUCTURE STRINGS
 async function reconstructStrings(name) {
@@ -119,6 +121,16 @@ function getModifiedIndexNumber(on_page_num, switched) {
     return result;
 }
 
+// DETECTING MOBILE DEVICES 
+function mobileDevices() {
+
+    // device detection
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4)))
+        return true;
+    else return false;
+}
+
 // WIKIPEDIA
 async function getWikipedia(param, turns) {
     
@@ -163,24 +175,114 @@ async function getWikipedia(param, turns) {
     }
 }
 
+function chartData(recipes, total_vals, Xlabels, daily_vals, num_case) {
+
+    // destroy old charts to avoid glitchy effects
+    // from overlaying charts created by creating instances of charts
+    if (num_case === 1) myChart.destroy();
+    else {
+
+        // divide the total number of recipes into individual one
+        const divided = total_vals.length / recipes;
+
+        // default value for the display, in order to show the first recipe
+        total_vals = total_vals.slice(0, divided);
+        Xlabels = Xlabels.slice(0, divided);
+
+        // plant_detail.style.overflowY = 'scroll';
+
+        nutrientsChart.style.display = 'block';
+
+        myChart = new Chart(nutrientsChart, {
+            type: 'bar',
+            data: {
+                labels: Xlabels,
+                datasets: [{
+                    label: 'Nutrient Values',
+                    data: total_vals,
+                    backgroundColor: 'rgba(255, 255, 255, 0.75)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                layout: {
+                    padding: {
+                        left: 0,
+                        right: 0,
+                        top: 10,
+                        bottom: 10
+                    }
+                },
+                legend: {
+                    display: true,
+                    labels: {
+                        align: 'center'
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            fontSize: 9
+                        }
+                    }]
+                }
+            }
+        });
+    }
+}
+
 // SHOW RECIPE ONCE THE NOTIFICATION IS CLICKED
 function showRecipe(recipe) {
+
+    let nutrients_labels = [],
+        nutrients_values = [],
+        nutrients_daily = [];
 
     // check for a valid length of the array
     if (recipe.hits.length > 0) {
         console.log(recipe);
 
         const childNodes = plant_detail.childNodes;
-        
-        childNodes.forEach(node => {
-            node.style !== undefined ? (node.style.left = '-1000%', node.style.position = 'absolute') :
-                console.log(node);
-        });
 
+        childNodes.forEach(node => {
+
+            // if a node's style is defined
+            node.style !== undefined ? 
+
+                // if the node's id is table_wrapper
+                node.id === 'table_wrapper' ? 
+                    (node.style.right = '-100%', node.style.position = 'absolute') :
+
+                    // or if the node's id is nutrientsChart
+                node.id === 'nutrientsChart' ? 
+                    (node.style.bottom = '0px') :
+
+                    // otherwise
+                    (node.style.left = '-100%') :
+            undefined;
+        });
+        
+        // loop through the array
         for (let i = 0; i < recipe.hits.length; i++) {
 
             const calories = recipe.hits[i].recipe.calories + 'kcal / serving';
+            const img_url = recipe.hits[i].recipe.image;
+            const digest = recipe.hits[i].recipe.digest;
+
+            // loop through digest array
+            for(let j = 0; j < digest.length; j++) {
+                
+                nutrients_labels.push(digest[j].label);
+                nutrients_values.push(digest[j].total);
+                nutrients_daily.push(digest[j].daily);
+            }
         }
+
+        // draw a chart with the total number of recipes,
+        // all recipe's total values, all the corresponding labels, and healthy daily consumption of the nutrients
+        chartData(recipe.hits.length, nutrients_values, nutrients_labels, nutrients_daily, 2);
     }
 }
 
@@ -209,15 +311,22 @@ function showRecipeNotification(recipe) {
     // if recipe is NOT a null obj
     recipe !== null ?
 
-        // if the length of the object array is valid
-        recipe.hits.length > 0 ? 
+        recipe.hits !== undefined ?
 
-            // display the toaster, with provided options
-            toastr.success('Click here to see more', `There is ${recipe.hits.length} recipe(s) for this plant`, toastr.options) :
-            toastr.warning(`There is ${recipe.hits.length} recipe(s) for this plant`, toastr.options) :
+            // if the length of the object array is valid
+            recipe.hits.length > 0 ? 
 
-        // otherwise, recipe is a null obj
-        toastr.danger('Cannot get the data at the moment', 'Error', toastr.options);
+                // if the length is only 1
+                recipe.hits.length === 1 ?
+
+                    // display the toaster, with provided options
+                    toastr.success('Click here to see more', `There is ${recipe.hits.length} recipe for this plant`, toastr.options) :
+                    toastr.success('Click here to see more', `There are ${recipe.hits.length} recipes for this plant`, toastr.options) :
+            toastr.warning(`There is ${recipe.hits.length} recipe for this plant`, toastr.options) :
+        toastr.error('Cannot get the data at the moment', 'Error', toastr.options):
+
+    // otherwise, recipe is a null obj
+    toastr.error('Cannot get the data at the moment', 'Error', toastr.options);
 }
 
 // EDAMAM (RECIPE API)
@@ -372,10 +481,25 @@ async function showPlantDetails(detail, img_url) {
 
     // BRING THE DETAILS DOWN IF A USER TRIES TO SEE OTHER INFO AFTER CLICKING THE TOASTR
     const childNodes = plant_detail.childNodes;
+    
+    // the default styling
     childNodes.forEach(node => {
-        node.style !== undefined ? (node.style.left = '0', node.style.position = 'relative') :
-            console.log(node)
+        node.style !== undefined ? 
+        
+            // if the node's id is table_wrapper
+            node.id === 'table_wrapper' ? 
+                (node.style.right = '0', node.style.position = 'relative') :
+
+            // or if the node's id is nutrientsChart
+            node.id === 'nutrientsChart' ? 
+                (node.style.bottom = '-100%') :
+                
+                // otherwise
+                (node.style.left = '0') :
+        undefined;
     });
+
+    plant_detail.style.overflow = 'hidden';
 
     // select old div elements
     const oldDivs = document.querySelectorAll('div'),
@@ -449,6 +573,15 @@ async function showPlantDetails(detail, img_url) {
             // hide all the detail-related elements
             plant_detail.style.display = 'none';
             plant_detail_wrapper.style.display = 'none';
+
+            // look for a valid class name of chartjs, to see if it's appended to the DOM
+            document.querySelector('.chartjs-size-monitor') !== null ? 
+
+                // if there is, destroy the chart
+                chartData(null, null, null, null, 1) :
+            
+            // otherwise, if a chart is not displayed
+            undefined;
         }
     });
 }
@@ -483,6 +616,10 @@ function listPages(total_plants, on_page_num) {
         limit_bounds = [-1, 4];
     let old_pages = document.querySelectorAll('button');
     let switched = false;
+
+    // hide all the detail-related elements
+    plant_detail.style.display = 'none';
+    plant_detail_wrapper.style.display = 'none';
 
     // check for any old buttons before loading new ones
     if (old_pages.length > 0) {
